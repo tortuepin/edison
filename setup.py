a009986ddadf5c13a709931d1f99895ef6d63cba
tortuepin <kameyasu119@gmail.com>
2017-05-03 12:25:11 +0900

    add install_require

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(
     name="edison",
     version='1,0',
+    install_requires=[
+        'Click',
+        'toml',
+        ],
     entry_points='''
         [console_scripts]
         edison=interface:cli
